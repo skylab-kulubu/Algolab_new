2c03d678336e86dd08004ea0556661f5c8e27d5a
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2023-07-07 17:07:33 +0300

    Update Binary_Search.cpp

--- Algo Bundle/Binary Search/Binary_Search.cpp
+++ Algo Bundle/Binary Search/Binary_Search.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 #define N 5
+using namespace std;
 /*
 Binary Search is defined as a searching algorithm used in a sorted array by repeatedly dividing the search interval in half. 
 The idea of binary search is to use the information that the array is sorted and reduce the time complexity to O(log N).
